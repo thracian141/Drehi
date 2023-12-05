34906986064f29c03f154347969a9b2f3c152d51
neeksan <krysteffsm@gmail.com>
2023-12-05 09:49:41 +0200

    COMMIT MAX MOG MOG MOGraboti

--- Svelte/src/routes/+page.svelte
+++ Svelte/src/routes/+page.svelte
@@ -1,50 +1,97 @@
 <script lang="ts">
-    let thomas = 'src/lib/img/thomas.png';
+    let thomas = "src/lib/img/thomas.png";
 </script>
 
-<div class="carousel">
-    <a style="filter:invert(100%); height:12rem;" href="/"><img src="/buttonicons/chevron-left.svg" alt=""></a>
-    <button>right</button>
-    <div class="current-image">
-        <img src={thomas} alt="" />
-        <span><b>ТЕСТОВО СЪОБЩЕНИЕ</b></span>
+<div class="container">
+    <div class="carousel">
+      <a href="/" class="carousel__face"><span>Thomas Jefferson 1876's</span></a>
+      <a href="/" class="carousel__face"><span>Кърджали "Специалки"</span></a>
+      <a href="/" class="carousel__face"><span>Vasil Versace</span></a>
+      <a href="/" class="carousel__face"><span>Дърва за огрев</span></a>
+      <a href="/" class="carousel__face"><span>UAE Polo</span></a>
+      <a href="/" class="carousel__face"><span>Hamburger</span></a>
+      <a href="/" class="carousel__face"><span>Мъжки Nike Корсет</span></a>
+      <a href="/" class="carousel__face"><span>Водолазен костюм Calvin Klein</span></a>
+      <a href="/" class="carousel__face"><span>Motherfuckin' CLOCK</span></a>
     </div>
-</div>
+  </div>
 
 <style>
-    .carousel {
-        background-color: rgb(49,51,56);
-        background-color: red;
-        display: flex;
-        justify-content: center;
-        align-items: flex-start;
-        width: 80rem;
-        height:32rem;
+.container {
+  position: relative;
+  width: 320px;
+  margin: 1rem auto 0 auto;
+  perspective: 1100px;
+}
 
-    }
-    .current-image {
-        display: flex;
-        flex-direction: column;
-        height:100%;
-        position: relative;
-    }
-    .current-image > img {
-        height:100%;
-        width:auto;
-        z-index: 1;
-    }
-    .current-image > span {
-        position: absolute;
-        bottom:0%;
-        left:50%;
-        transform: translateX(-50%);
-        width:100%;
-        height:20%;
-        background-color: rgba(0,0,0,0.4);
-        backdrop-filter: blur(4px);
-        text-align: center;
-        font-size: 2.4rem;
-        line-height: 6rem;
-        z-index: 2;
-    }
+.carousel {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  transform-style: preserve-3d; 
+  animation: rotate360 40s infinite forwards linear;
+}
+.carousel__face { 
+  position: absolute;
+  width: 300px;
+  height: 187px;
+  top: 20px;
+  left: 10px;
+  right: 10px;
+  background-size: cover;
+  box-shadow:inset 0 0 0 2000px rgba(0,0,0,0.5);
+  display: flex;
+  transition:box-shadow 0.1s ease-in-out;
+}
+.carousel__face, .carousel__face:visited {
+    color: white;
+    text-decoration: none;
+}
+.carousel__face:hover {
+    box-shadow: inset 0 0 0 2000px rgba(0,0,0,0.2);
+}
+
+span {
+  margin: auto;
+  font-size: 2rem;
+}
+
+.carousel__face:nth-child(1) {
+  background-image: url("src/lib/img/shoes.png");
+  transform: rotateY(  0deg) translateZ(430px); }
+.carousel__face:nth-child(2) { 
+    background-image: url("src/lib/img/specialki.jpg");
+    transform: rotateY( 40deg) translateZ(430px); }
+.carousel__face:nth-child(3) {
+    background-image: url("src/lib/img/qke.jpg");
+  transform: rotateY( 80deg) translateZ(430px); }
+.carousel__face:nth-child(4) {
+    background-image: url("src/lib/img/dyrva.jpg");
+  transform: rotateY(120deg) translateZ(430px); }
+.carousel__face:nth-child(5) { 
+    background-image: url("src/lib/img/arab.jpg");
+ transform: rotateY(160deg) translateZ(430px); }
+.carousel__face:nth-child(6) { 
+    background-image: url("src/lib/img/hamburg.webp");
+ transform: rotateY(200deg) translateZ(430px); }
+.carousel__face:nth-child(7) { 
+    background-image: url("src/lib/img/corset.avif");
+ transform: rotateY(240deg) translateZ(430px); }
+.carousel__face:nth-child(8) {
+    background-image: url("src/lib/img/shoes.png");
+  transform: rotateY(280deg) translateZ(430px); }
+.carousel__face:nth-child(9) {
+    background-image: url("src/lib/img/clock.jpg");
+  transform: rotateY(320deg) translateZ(430px); }
+
+
+
+@keyframes rotate360 {
+  from {
+    transform: rotateY(0deg);
+  }
+  to {
+    transform: rotateY(-360deg);
+  }
+}
 </style>
--- Svelte/src/routes/Navbar.svelte
+++ Svelte/src/routes/Navbar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { fade, slide } from 'svelte/transition';
+    import { fade, slide } from "svelte/transition";
     let dropdownEnabled = false;
     function enableDropdown() {
         dropdownEnabled = true;
@@ -13,185 +13,224 @@
     <div style="display: flex; flex-direction:row; gap:2rem;">
         <a>Доставка до Овча Купел</a>
         <a>2-часов срок за връщане</a>
-        <a href="https://www.facebook.com/boyko.borissov.7/?locale=bg_BG">Връзка с Бойко Борисов</a>
+        <a href="https://www.facebook.com/boyko.borissov.7/?locale=bg_BG"
+            >Връзка с Бойко Борисов</a
+        >
     </div>
-    <div style="display:flex; flex-direction:row; align-items:center; gap:0.3rem;">
-        <img src="/buttonicons/envelope-at-fill.svg" alt="contact" style="width:1.2rem; height:1.2rem; filter: brightness(0) saturate(100%) invert(99%) sepia(1%) saturate(6197%) hue-rotate(301deg) brightness(110%) contrast(101%);" />
+    <div
+        style="display:flex; flex-direction:row; align-items:center; gap:0.3rem;"
+    >
+        <img
+            src="/buttonicons/envelope-at-fill.svg"
+            alt="contact"
+            style="width:1.2rem; height:1.2rem; filter: brightness(0) saturate(100%) invert(99%) sepia(1%) saturate(6197%) hue-rotate(301deg) brightness(110%) contrast(101%);"
+        />
         <p>vasiljavaskriptin@gov.ru</p>
     </div>
 </div>
 <nav>
     <div id="logodiv">
-        <img src="/logo.webp" alt="logo" id="logo"/>
+        <img src="/logo.webp" alt="logo" id="logo" />
         <h1 style="font-size: 3rem; position:absolute; right:0%;">рехи</h1>
     </div>
 
-    <div style="display: flex; flex-direction:row; background-color:#5663F7; align-items:center; border-radius:0.6rem; width:41.5rem;">
+    <div
+        style="display: flex; flex-direction:row; background-color:#5663F7; align-items:center; border-radius:0.6rem; width:41.5rem;"
+    >
         <input type="text" id="finder" placeholder="Търси Dрехи..." />
-        <img src="/buttonicons/search.svg" alt="search" id="finderico"/>
-    </div>    
+        <img src="/buttonicons/search.svg" alt="search" id="finderico" />
+    </div>
 
     <div style="display:flex;flex-direction:row; gap:1.2rem;">
         <a href="/" class="navbutton">
-            <img src="/buttonicons/heart-half.svg" alt="liked items"/>
+            <img src="/buttonicons/heart-half.svg" alt="liked items" />
             <p style="margin:0; padding:0;">ХАРЕСАНИ</p>
         </a>
         <a href="/" class="navbutton">
-            <img src="/buttonicons/cart-fill.svg" alt="liked items"/>
+            <img src="/buttonicons/cart-fill.svg" alt="liked items" />
             <p style="margin:0; padding:0;">КОЛИЧКА</p>
         </a>
         <a href="/" class="navbutton">
-            <img src="/buttonicons/person.svg" alt="liked items"/>
+            <img src="/buttonicons/person.svg" alt="liked items" />
             <p style="margin:0; padding:0;">ПРОФИЛ</p>
         </a>
     </div>
 </nav>
 <div class="bottomnav">
-    <button on:mouseenter={()=> dropdownEnabled = true} on:mouseleave={()=> dropdownEnabled = false} class="bottom-nav-button">
+    <button
+        on:mouseenter={() => (dropdownEnabled = true)}
+        on:mouseleave={() => (dropdownEnabled = false)}
+        class="bottom-nav-button"
+    >
         МЪЖКИ
         <div class="bottomhider"></div>
     </button>
-    <button on:mouseenter={()=> dropdownEnabled = true} on:mouseleave={()=> dropdownEnabled = false} class="bottom-nav-button">
+    <button
+        on:mouseenter={() => (dropdownEnabled = true)}
+        on:mouseleave={() => (dropdownEnabled = false)}
+        class="bottom-nav-button"
+    >
         ДАМСКИ
         <div class="bottomhider"></div>
     </button>
-    <button on:mouseenter={()=> dropdownEnabled = true} on:mouseleave={()=> dropdownEnabled = false} class="bottom-nav-button">
+    <button
+        on:mouseenter={() => (dropdownEnabled = true)}
+        on:mouseleave={() => (dropdownEnabled = false)}
+        class="bottom-nav-button"
+    >
         ДЕТСКИ
         <div class="bottomhider"></div>
     </button>
-    <button on:mouseenter={()=> dropdownEnabled = true} on:mouseleave={()=> dropdownEnabled = false} class="bottom-nav-button">
+    <button
+        on:mouseenter={() => (dropdownEnabled = true)}
+        on:mouseleave={() => (dropdownEnabled = false)}
+        class="bottom-nav-button"
+    >
         АКСЕСОАРИ
         <div class="bottomhider"></div>
     </button>
-    <button on:mouseenter={()=> dropdownEnabled = true} on:mouseleave={()=> dropdownEnabled = false} class="bottom-nav-button">
+    <button
+        on:mouseenter={() => (dropdownEnabled = true)}
+        on:mouseleave={() => (dropdownEnabled = false)}
+        class="bottom-nav-button">
         MERCH
         <div class="bottomhider"></div>
     </button>
-    <button on:mouseenter={()=> dropdownEnabled = true} on:mouseleave={()=> dropdownEnabled = false} class="bottom-nav-button">
+    <button
+        on:mouseenter={() => (dropdownEnabled = true)}
+        on:mouseleave={() => (dropdownEnabled = false)}
+        class="bottom-nav-button">
         МАРКИ
         <div class="bottomhider"></div>
     </button>
-    <button on:mouseenter={()=> dropdownEnabled = true} on:mouseleave={()=> dropdownEnabled = false} class="bottom-nav-button">
+    <button
+        on:mouseenter={() => (dropdownEnabled = true)}
+        on:mouseleave={() => (dropdownEnabled = false)}
+        class="bottom-nav-button"
+    >
         ДРУГИ
         <div class="bottomhider"></div>
     </button>
     {#if dropdownEnabled}
-        <div in:slide={{duration: 800}} out:slide={{duration:400}} class="bottomnav-dropdown">
+        <div in:slide={{ duration: 800 }} out:slide={{ duration: 400 }} class="bottomnav-dropdown">
             <!-- Dropdown content goes here -->
         </div>
     {/if}
 </div>
 
-
 <style>
     .bottomnav-dropdown {
-        display: flex;
         position: absolute;
-        top:100%;
-        background-color: rgb(43,45,49);
+        top: 100%;
+        background-color: rgb(43, 45, 49);
         width: calc(100% - 48rem);
         height: 40vh;
-        outline: #5663F7 solid 3px;
+        outline: #5663f7 solid 3px;
         border-radius: 0 0 0.3rem 0.3rem;
-        padding:3rem;
+        padding: 3rem;
         z-index: 500;
     }
     .bottomhider {
         opacity: 0;
         display: flex;
-        background-color: rgb(43,45,49);
-        height:7px;
-        width:100%;
+        background-color: rgb(43, 45, 49);
+        height: 7px;
+        width: 100%;
         position: absolute;
-        bottom:-3px;
-        left:0;
+        bottom: -3px;
+        left: 0;
         z-index: 999;
     }
     .bottom-nav-button {
         display: flex;
         flex-direction: column;
-        height:100%;
+        height: 100%;
         flex-grow: 1;
         border-radius: 0.3rem;
-        background-color: rgb(30,31,34);
+        background-color: rgb(30, 31, 34);
         border: none;
         transition: filter 0.1s ease-in-out;
         align-items: center;
         justify-content: center;
-        color: rgb(128,132,142);
-        font-size: 1.6rem;
+        color: rgb(128, 132, 142);
+        font-size: 1.4rem;
         font-weight: bold;
         position: relative;
     }
     .bottom-nav-button:hover {
         z-index: 888;
-        background-color: rgb(43,45,49);
-        cursor:pointer;
-        outline: #5663F7 solid 3px;
-        
+        background-color: rgb(43, 45, 49);
+        cursor: pointer;
+        outline: #5663f7 solid 3px;
     }
     .bottom-nav-button:hover > .bottomhider {
-        opacity:1;
+        opacity: 1;
     }
     .bottomnav {
         display: flex;
         flex-direction: row;
         justify-content: space-between;
         align-items: center;
-        background-color: rgb(30,31,34);
+        background-color: rgb(30, 31, 34);
         height: 6rem;
-        padding: 0 24rem;
+        padding: 0 20%;
         box-shadow: rgba(0, 0, 0, 0.2) 0px -2px 3px 0.5px;
-        border-bottom:1px solid #424549dd;
+        border-bottom: 1px solid #424549dd;
         position: relative;
     }
     #finder {
         border-radius: 0.5rem;
-        background-color: rgb(30,31,34);
+        background-color: rgb(30, 31, 34);
         width: 36rem;
-        height:4rem;
+        height: 4rem;
         border: none;
         font-size: 1.4rem;
         padding: 0 0.6rem;
-        color:white;
+        color: white;
         transition: filter 0.2s ease-in-out;
     }
     #finder:focus {
         outline: none;
-        filter:drop-shadow(0px 0px 4px rgba(255, 255, 255, 0.5));
+        filter: drop-shadow(0px 0px 4px rgba(255, 255, 255, 0.5));
     }
     #finder::placeholder {
-        color: rgb(127,133,141);
+        color: rgb(127, 133, 141);
     }
     #finderico {
         margin-left: 1rem;
         height: 2rem;
         width: auto;
-        filter: brightness(0) saturate(100%) invert(100%) sepia(100%) saturate(38%) hue-rotate(321deg) brightness(110%) contrast(110%);
+        filter: brightness(0) saturate(100%) invert(100%) sepia(100%)
+            saturate(38%) hue-rotate(321deg) brightness(110%) contrast(110%);
         transition: filter 0.1s ease-in-out;
     }
     #finderico:hover {
         cursor: pointer;
-        filter: brightness(0) saturate(100%) invert(100%) sepia(100%) saturate(38%) hue-rotate(321deg) brightness(110%) contrast(110%) drop-shadow(0px 0px 4px rgba(255, 255, 255, 0.8));
+        filter: brightness(0) saturate(100%) invert(100%) sepia(100%)
+            saturate(38%) hue-rotate(321deg) brightness(110%) contrast(110%)
+            drop-shadow(0px 0px 4px rgba(255, 255, 255, 0.8));
     }
     .navbutton {
         display: flex;
         flex-direction: column;
         align-items: center;
-        gap:0.3rem;
+        gap: 0.3rem;
         text-decoration: none;
         transition: filter 0.1s ease-in-out;
-        filter: brightness(0) saturate(100%) invert(78%) sepia(12%) saturate(362%) hue-rotate(160deg) brightness(85%) contrast(87%);
+        filter: brightness(0) saturate(100%) invert(78%) sepia(12%)
+            saturate(362%) hue-rotate(160deg) brightness(85%) contrast(87%);
     }
     .navbutton > p {
         font-size: 1.2rem;
     }
     .navbutton > img {
         height: 1.7rem;
-        width:auto;
+        width: auto;
     }
     .navbutton:hover {
-        filter: brightness(0) saturate(100%) invert(100%) sepia(100%) saturate(38%) hue-rotate(321deg) brightness(110%) contrast(110%) drop-shadow(0px 0px 3px rgba(255, 255, 255, 0.65));;
+        filter: brightness(0) saturate(100%) invert(100%) sepia(100%)
+            saturate(38%) hue-rotate(321deg) brightness(110%) contrast(110%)
+            drop-shadow(0px 0px 3px rgba(255, 255, 255, 0.65));
     }
     #logodiv {
         display: flex;
@@ -201,40 +240,40 @@
         width: 11.5rem;
     }
     #logo {
-        height:5.5rem;
+        height: 5.5rem;
         width: auto;
-        margin:0;
+        margin: 0;
         position: absolute;
         left: 0;
     }
     p {
-        color:white;
+        color: white;
     }
     .topnav {
         display: flex;
         flex-direction: row;
-        color: rgb(128,132,142);
+        color: rgb(128, 132, 142);
         line-height: 3rem;
-        background-color: rgb(30,31,34);
+        background-color: rgb(30, 31, 34);
         height: 2.6rem;
         padding: 0 2rem;
         align-items: center;
         justify-content: space-between;
         box-shadow: rgba(0, 0, 0, 0.25) 0px 0px 4px 1px;
         z-index: 999;
-        border-bottom:1px solid #424549dd;
+        border-bottom: 1px solid #424549dd;
     }
     .topnav > div > a {
-        color:rgb(128,132,142);
+        color: rgb(128, 132, 142);
         transition: color 0.1s ease-in-out;
-        cursor:grab;
+        cursor: grab;
         text-decoration: none !important;
     }
     .topnav > div > a:hover {
         color: white !important;
     }
     .topnav > div > a:visited {
-        color:rgb(128,132,142);
+        color: rgb(128, 132, 142);
         text-decoration: none !important;
     }
     .topnav > div > a:visited:hover {
@@ -245,10 +284,10 @@
         flex-direction: row;
         height: 10rem;
         flex-grow: 1;
-        background-color: rgb(43,45,49);
+        background-color: rgb(43, 45, 49);
         justify-content: space-between;
         align-items: center;
         padding: 0 2rem;
-        border-bottom:1px solid #424549dd;
+        border-bottom: 1px solid #424549dd;
     }
 </style>
